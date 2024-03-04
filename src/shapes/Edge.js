e9748bfdf0d59ba4bd2e9bc536418cf58401932e
Ditmar <ditmarcastro@gmail.com>
2024-03-03 22:38:36 -0400

    feat: added new base

---
+++ src/shapes/Edge.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+class Edge extends THREE.Line {
+    constructor(nodeStart, nodeEnd, material) {
+        const geometry = new THREE.BufferGeometry().setFromPoints([nodeStart.position, nodeEnd.position]);
+        super(geometry, material);
+        this.nodeStart = nodeStart;
+        this.nodeEnd = nodeEnd;
+    }
+
+    // Método para actualizar la posición de la arista basada en los nodos
+    updatePosition() {
+        this.geometry.setFromPoints([this.nodeStart.position, this.nodeEnd.position]);
+        this.geometry.attributes.position.needsUpdate = true; // Necesario para actualizar la geometría
+    }
+}
