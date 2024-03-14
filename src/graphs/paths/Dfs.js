78e00eb8d3d9ff55bec944bccc459da734e566b7
EdwinCj04 <cristiancruzjaita@gmail.com>
2024-03-14 12:52:49 -0400

    HOMEWORKONE

--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,6 +1,21 @@
 // Homework, this presentation will be on fridays
 // in lab.
-const dfs = () => {
-    
-}
+const dfs = (source) => {
+    const stack = [];
+    source.setVisited(true);
+    stack.push(source);
+
+    while (stack.length !== 0) {
+        const currentVertex = stack.pop();
+        currentVertex.print();
+
+        currentVertex.getVertexList().forEach(vertex => {
+            if (!vertex.getVisited()) {
+                vertex.setVisited(true);
+                stack.push(vertex);
+            }
+        });
+    }
+};
+
 export default dfs;
