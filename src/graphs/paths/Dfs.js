a9ef111cd4ff3af257975698a81758544d64f2b4
MarcoCruz <marco123xxz@gmail.com>
2024-03-14 23:17:51 -0400

    feat: added dfs

--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,6 +1,20 @@
 // Homework, this presentation will be on fridays
 // in lab.
-const dfs = () => {
-    
+const dfs = (source) => {
+    const vertexStack = [];
+    source.setVisited(true);
+    vertexStack.push(source);
+
+    while (vertexStack.length !== 0) {
+        const currentVertex = vertexStack.pop();
+        currentVertex.print();
+        
+        currentVertex.getVertexList().forEach(neighbor => {
+            if (!neighbor.getVisited()) {
+                neighbor.setVisited(true);
+                vertexStack.push(neighbor);
+            }
+        });
+    }
 }
 export default dfs;
