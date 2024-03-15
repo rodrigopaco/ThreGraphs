e08987b40d840a8a6f5385111cb2d5ab07da6525
Jorge <chavajorge5@gmail.com>
2024-03-14 22:10:24 -0400

    feat: added bfs

--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,6 +1,23 @@
 // Homework, this presentation will be on fridays
 // in lab.
-const dfs = () => {
-    
-}
+const dfs = (source) => {
+    source.setVisited(true);
+    const vertexStack = [];
+    vertexStack.push(source);
+    while (!isEmpty(vertexStack)) {
+        const currentVertex = vertexStack.pop();
+        currentVertex.print();
+        currentVertex.getVertexList().forEach(neighbor => {
+            if (!neighbor.getVisited()) {
+                neighbor.setVisited(true);
+                vertexStack.push(neighbor);
+            }
+        });
+    }
+};
+
+const isEmpty = (vertexStack) => {
+    return vertexStack.length === 0;
+};
+
 export default dfs;
