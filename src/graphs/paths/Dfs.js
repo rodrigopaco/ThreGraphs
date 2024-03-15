237e19b88e6fa79da43c6fcaf352f04a48f61d4b
Jorge <chavajorge5@gmail.com>
2024-03-14 23:04:22 -0400

    fallo

--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,23 +1,10 @@
 // Homework, this presentation will be on fridays
 // in lab.
-const dfs = (source) => {
-    source.setVisited(true);
-    const vertexStack = [];
-    vertexStack.push(source);
-    while (!isEmpty(vertexStack)) {
-        const currentVertex = vertexStack.pop();
-        currentVertex.print();
-        currentVertex.getVertexList().forEach(neighbor => {
-            if (!neighbor.getVisited()) {
-                neighbor.setVisited(true);
-                vertexStack.push(neighbor);
-            }
-        });
-    }
+const dfs = () => {
+   
 };
 
-const isEmpty = (vertexStack) => {
-    return vertexStack.length === 0;
-};
+
+
 
 export default dfs;
