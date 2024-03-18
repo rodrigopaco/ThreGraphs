4abe4a077e488a28f8a4a83b09c5ae9eb456aa23
Ditmar <spyatorio@gmail.com>
2024-03-18 08:23:41 -0400

    Revert "Merge pull request #7 from Ditmar/HOMEWORK-EDWINCRUZJAITA"
    This reverts commit 99188184f3ca91411f0a1b7648b5beced7ec8f21, reversing
    changes made to 237e19b88e6fa79da43c6fcaf352f04a48f61d4b.

--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,25 +1,5 @@
 // Homework, this presentation will be on fridays
 // in lab.
-HOMEWORK-EDWINCRUZJAITA
-const dfs = (source) => {
-    const stack = [];
-    source.setVisited(true);
-    stack.push(source);
-
-    while (stack.length !== 0) {
-        const currentVertex = stack.pop();
-        currentVertex.print();
-
-        currentVertex.getVertexList().forEach(vertex => {
-            if (!vertex.getVisited()) {
-                vertex.setVisited(true);
-                stack.push(vertex);
-            }
-        });
-    }
-};
-
-export default dfs;
 const dfs = () => {
    
 };
@@ -28,4 +8,3 @@ const dfs = () => {
 
 
 export default dfs;
-master
